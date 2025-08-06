af4b2ba97b1287b3a500bebf42eb88deeaab3709
ᴠɪʙє ʙσᴛs [🇮🇳] <sanatanixapi@gmail.com>
2025-08-06 08:29:46 +0530

    Initial commit

---
+++ AnonMusic/plugins/tools/ping.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from AnonMusic import app
+from AnonMusic.core.call import Anony
+from AnonMusic.utils import bot_sys_stats
+from AnonMusic.utils.decorators.language import language
+from AnonMusic.utils.inline import supp_markup
+from config import BANNED_USERS, PING_IMG_URL
+
+
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    start = datetime.now()
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        has_spoiler=True,
+        caption=_["ping_1"].format(app.mention),
+    )
+    pytgping = await Anony.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(_),
+    )
